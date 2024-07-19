456a12aa1dfa6f2815ed0cfbc446932d6daa852b
DusterFan668 <copelandc6@gmail.com>
2024-07-18 18:06:41 -0700

    add frontend

--- backend/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Account(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
